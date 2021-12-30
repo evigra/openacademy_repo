c93976afd17e09c3b598addcdc570ccf282174ad
Eduardo Vizcaino <eduardo.vizcano@vauxoo.com>
2021-12-30 09:14:11 -0600

    [REF] Compute fields and defaults

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -18,7 +18,10 @@
 #             record.value2 = float(record.value) / 100
 
 
-from odoo import models, fields, api
+from odoo import models, fields, api, time
+
+def get_uid(self, *a):
+    return self.env.uid
 
 class Course(models.Model):
     _name = 'openacademy.course'
@@ -26,21 +29,36 @@ class Course(models.Model):
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
-    responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
+    responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True,
+        default=lambda self, *a: self.env.uid,
+        #default=get_uid
+        )
 
-class Session(models.Model):
+class Session(models.Model):    
     _name = 'openacademy.session'
+    _description = 'Model to store session'
 
     name = fields.Char(required=True)
-    start_date = fields.Date()
-    duration = fields.Float(digits=(6, 2), help="Duration in days")
-    seats = fields.Integer(string="Number of seats")    
-    instructor_id = fields.Many2one('res.partner', string="Instructor")
-    course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
-    attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    #start_date = fields.Date(default=fields.Date.today)
+    start_date = fields.Date(default=lambda *a:time.strftime('%Y-%m-%d %H:%M:%S'))
+    datetime_test = fields.Datetime(default=fields.Datetime.now)
+    duration = fields.Float(digits=(6, 2), help="Duration in days")    
+    seats = fields.Integer(string="Number of seats")
+    instructor_id = fields.Many2one('res.partner', string="Instructor", domain=['|', ('instructor', '=', True),                      ('category_id.name', 'ilike', "Teacher")])
+    course_id = fields.Many2one('openacademy.course',   ondelete='cascade', string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")    
+    taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+    active = fields.Boolean(default=True) 
     
     
-    
+    @api.depends('seats', 'attendee_ids')
+    def _taken_seats(self):
+        #import pdb; pdb.set_trace()
+        for r in self:
+            if not r.seats:
+                r.taken_seats = 0.0
+            else:
+                r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
     
     
     
