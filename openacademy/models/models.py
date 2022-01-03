733b82ab61f82020ab57a9cda0cb7d6d5bbe411b
Eduardo Vizcaino <eduardo.vizcano@vauxoo.com>
2022-01-03 10:55:23 -0600

    [REF] Advanced views

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -18,7 +18,8 @@
 #             record.value2 = float(record.value) / 100
 
 
-from odoo import models, fields, api, time
+from odoo import models, fields, api, time, exceptions
+from datetime import timedelta 
 
 def get_uid(self, *a):
     return self.env.uid
@@ -27,12 +28,39 @@ class Course(models.Model):
     _name = 'openacademy.course'
     _description = 'Model to store course'
 
+
+
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
     responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True,
         default=lambda self, *a: self.env.uid,
         #default=get_uid
         )
+
+    def copy(self, default=None):
+        default = dict(default or {})
+
+        copied_count = self.search_count(
+            [('name', '=like', u"Copy of {}%".format(self.name))])
+        if not copied_count:
+            new_name = u"Copy of {}".format(self.name)
+        else:
+            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+
+        default['name'] = new_name
+        return super(Course, self).copy(default)
+
+
+    _sql_constraints = [
+        ('name_description_check',
+         'CHECK(name != description)',
+         "The title of the course should not be the description"),
+
+        ('name_unique',
+         'UNIQUE(name)',
+         "The course title must be unique"),
+    ]
+
 
 class Session(models.Model):    
     _name = 'openacademy.session'
@@ -49,8 +77,48 @@ class Session(models.Model):
     attendee_ids = fields.Many2many('res.partner', string="Attendees")    
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
     active = fields.Boolean(default=True) 
+    end_date = fields.Date(string="End Date", store=True, compute='_get_end_date', inverse='_set_end_date')
+    attendees_count = fields.Integer( string="Attendees count", compute='_get_attendees_count', store=True)
+    color = fields.Integer()
+    hours = fields.Float(string="Duration in hours",
+                     compute='_get_hours', inverse='_set_hours')
+    def _get_hours(self):
+        for r in self:
+            r.hours = r.duration * 24
+
+    def _set_hours(self):
+        for r in self:
+            r.duration = r.hours / 24
     
-    
+    @api.depends('attendee_ids')
+    def _get_attendees_count(self):
+        for r in self:
+            r.attendees_count = len(r.attendee_ids)
+
+
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        for r in self:
+            if not (r.start_date and r.duration):
+                r.end_date = r.start_date
+                continue
+
+            # Add duration to start_date, but: Monday + 5 days = Saturday, so
+            # subtract one second to get on Friday instead
+            start = fields.Datetime.from_string(r.start_date)
+            duration = timedelta(days=r.duration, seconds=-1)
+            r.end_date = start + duration
+    def _set_end_date(self):
+        for r in self:
+            if not (r.start_date and r.end_date):
+                continue
+
+            # Compute the difference between dates, but: Friday - Monday = 4 days,
+            # so add one day to get 5 days instead
+            start_date = fields.Datetime.from_string(r.start_date)
+            end_date = fields.Datetime.from_string(r.end_date)
+            r.duration = (end_date - start_date).days + 1
+        
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
         #import pdb; pdb.set_trace()
@@ -60,5 +128,31 @@ class Session(models.Model):
             else:
                 r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
     
+    @api.onchange('seats', 'attendee_ids')
+    def _verify_valid_seats(self):
+        # set auto-changing field
+        if self.seats<0:
+            self.active = False
+            return {
+                'warning': {
+                    'title': "Incorrect 'seats' value",
+                    'message': "The number of available seats may not be negative",
+                }
+            }
+        if self.seats<len(self.attendee_ids):
+            self.active = False
+            return {
+                'warning': {
+                    'title': "To many attendees",
+                    'message': "Increase seats or remove excess attendees",
+                }
+            }               
+        self.active = True
+
+    @api.constrains('instructor_id', 'attendee_ids')
+    def _check_instructor_not_in_attendees(self):
+        for r in self:
+            if r.instructor_id and r.instructor_id in r.attendee_ids:
+                raise exceptions.ValidationError("A session's instructor can't be an attendee")
     
     
